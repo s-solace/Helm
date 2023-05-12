706a1f25a3c8ed6dcb83bcd1b15d801f2f60aac3
Akarsh <Akarsh@DESKTOP-D5A311B>
2023-05-11 22:42:37 -0700

    Add project files.

---
+++ Helm/Calendar.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Helm
+{
+    partial class Calendar
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel1 = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
+            daysOfTheWeekPanel = new Panel();
+            sundayLabel = new Label();
+            mondayLabel = new Label();
+            tuesdayLabel = new Label();
+            wednesdayLabel = new Label();
+            thursdayLabel = new Label();
+            fridayLabel = new Label();
+            saturdayLabel = new Label();
+            calendarPanel = new Panel();
+            panel1.SuspendLayout();
+            daysOfTheWeekPanel.SuspendLayout();
+            calendarPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("ROG Fonts", 45F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(423, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(234, 72);
+            label1.TabIndex = 0;
+            label1.Text = "Helm";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonShadow;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(7, 87);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(128, 248);
+            panel1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(0, 85);
+            button3.Name = "button3";
+            button3.Size = new Size(128, 47);
+            button3.TabIndex = 2;
+            button3.Text = "Resources";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(0, 44);
+            button2.Name = "button2";
+            button2.Size = new Size(128, 47);
+            button2.TabIndex = 1;
+            button2.Text = "Notes";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(128, 47);
+            button1.TabIndex = 0;
+            button1.Text = "Calendar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(343, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 37);
+            label2.TabIndex = 2;
+            label2.Text = "May";
+            // 
+            // daysOfTheWeekPanel
+            // 
+            daysOfTheWeekPanel.BackColor = SystemColors.AppWorkspace;
+            daysOfTheWeekPanel.Controls.Add(saturdayLabel);
+            daysOfTheWeekPanel.Controls.Add(fridayLabel);
+            daysOfTheWeekPanel.Controls.Add(thursdayLabel);
+            daysOfTheWeekPanel.Controls.Add(wednesdayLabel);
+            daysOfTheWeekPanel.Controls.Add(tuesdayLabel);
+            daysOfTheWeekPanel.Controls.Add(mondayLabel);
+            daysOfTheWeekPanel.Controls.Add(sundayLabel);
+            daysOfTheWeekPanel.Location = new Point(7, 54);
+            daysOfTheWeekPanel.Name = "daysOfTheWeekPanel";
+            daysOfTheWeekPanel.Size = new Size(809, 36);
+            daysOfTheWeekPanel.TabIndex = 3;
+            // 
+            // sundayLabel
+            // 
+            sundayLabel.AutoSize = true;
+            sundayLabel.BackColor = Color.Transparent;
+            sundayLabel.Font = new Font("Segoe UI", 17F, FontStyle.Regular, GraphicsUnit.Point);
+            sundayLabel.Location = new Point(1, 2);
+            sundayLabel.Name = "sundayLabel";
+            sundayLabel.Size = new Size(89, 31);
+            sundayLabel.TabIndex = 0;
+            sundayLabel.Text = "Sunday";
+            // 
+            // mondayLabel
+            // 
+            mondayLabel.AutoSize = true;
+            mondayLabel.BackColor = Color.Transparent;
+            mondayLabel.Font = new Font("Segoe UI", 17F, FontStyle.Regular, GraphicsUnit.Point);
+            mondayLabel.Location = new Point(93, 2);
+            mondayLabel.Name = "mondayLabel";
+            mondayLabel.Size = new Size(98, 31);
+            mondayLabel.TabIndex = 1;
+            mondayLabel.Text = "Monday";
+            // 
+            // tuesdayLabel
+            // 
+            tuesdayLabel.AutoSize = true;
+            tuesdayLabel.BackColor = Color.Transparent;
+            tuesdayLabel.Font = new Font("Segoe UI", 17F, FontStyle.Regular, GraphicsUnit.Point);
+            tuesdayLabel.Location = new Point(196, 3);
+            tuesdayLabel.Name = "tuesdayLabel";
+            tuesdayLabel.Size = new Size(98, 31);
+            tuesdayLabel.TabIndex = 4;
+            tuesdayLabel.Text = "Tuesday";
+            // 
+            // wednesdayLabel
+            // 
+            wednesdayLabel.AutoSize = true;
+            wednesdayLabel.BackColor = Color.Transparent;
+            wednesdayLabel.Font = new Font("Segoe UI", 17F, FontStyle.Regular, GraphicsUnit.Point);
+            wednesdayLabel.Location = new Point(295, 3);
+            wednesdayLabel.Name = "wednesdayLabel";
+            wednesdayLabel.Size = new Size(132, 31);
+            wednesdayLabel.TabIndex = 4;
+            wednesdayLabel.Text = "Wednesday";
+            // 
+            // thursdayLabel
+            // 
+            thursdayLabel.AutoSize = true;
+            thursdayLabel.BackColor = Color.Transparent;
+            thursdayLabel.Font = new Font("Segoe UI", 17F, FontStyle.Regular, GraphicsUnit.Point);
+            thursdayLabel.Location = new Point(430, 2);
+            thursdayLabel.Name = "thursdayLabel";
+            thursdayLabel.Size = new Size(107, 31);
+            thursdayLabel.TabIndex = 4;
+            thursdayLabel.Text = "Thursday";
+            // 
+            // fridayLabel
+            // 
+            fridayLabel.AutoSize = true;
+            fridayLabel.BackColor = Color.Transparent;
+            fridayLabel.Font = new Font("Segoe UI", 17F, FontStyle.Regular, GraphicsUnit.Point);
+            fridayLabel.Location = new Point(568, 3);
+            fridayLabel.Name = "fridayLabel";
+            fridayLabel.Size = new Size(76, 31);
+            fridayLabel.TabIndex = 4;
+            fridayLabel.Text = "Friday";
+            // 
+            // saturdayLabel
+            // 
+            saturdayLabel.AutoSize = true;
+            saturdayLabel.BackColor = Color.Transparent;
+            saturdayLabel.Font = new Font("Segoe UI", 17F, FontStyle.Regular, GraphicsUnit.Point);
+            saturdayLabel.Location = new Point(676, 2);
+            saturdayLabel.Name = "saturdayLabel";
+            saturdayLabel.Size = new Size(104, 31);
+            saturdayLabel.TabIndex = 4;
+            saturdayLabel.Text = "Saturday";
+            // 
+            // calendarPanel
+            // 
+            calendarPanel.Controls.Add(label2);
+            calendarPanel.Controls.Add(daysOfTheWeekPanel);
+            calendarPanel.Location = new Point(163, 86);
+            calendarPanel.Name = "calendarPanel";
+            calendarPanel.Size = new Size(822, 487);
+            calendarPanel.TabIndex = 4;
+            // 
+            // Calendar
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1170, 616);
+            Controls.Add(calendarPanel);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Name = "Calendar";
+            Text = "Helm";
+            panel1.ResumeLayout(false);
+            daysOfTheWeekPanel.ResumeLayout(false);
+            daysOfTheWeekPanel.PerformLayout();
+            calendarPanel.ResumeLayout(false);
+            calendarPanel.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label2;
+        private Panel daysOfTheWeekPanel;
+        private Label sundayLabel;
+        private Label saturdayLabel;
+        private Label fridayLabel;
+        private Label thursdayLabel;
+        private Label wednesdayLabel;
+        private Label tuesdayLabel;
+        private Label mondayLabel;
+        private Panel calendarPanel;
+    }
+}
---
+++ Helm/Calendar.cs
@@ -0,0 +1,11 @@
+namespace Helm
+{
+    public partial class Calendar : Form
+    {
+        public Calendar()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
